77baa0ea8572a7cc738f463a1ccebcf99548a298
sal794 <sam.luebbers@gmail.com>
2017-02-01 22:01:56 -0600

    Initial Project Setup.

---
+++ Drizzle/src/main/java/landing/App.java
@@ -0,0 +1,15 @@
+package landing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import landing.App;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
---
+++ Drizzle/src/main/java/landing/LandingController.java
@@ -0,0 +1,16 @@
+package landing;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LandingController {
+
+    @RequestMapping("/landing")
+    public String greeting(){
+        return "landing";
+    }
+
+}
